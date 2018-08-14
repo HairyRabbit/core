e1686fcea4d22a19ec9cfc94e349d9f032729168
Rabbit <yfhj1990@hotmail.com>
2018-08-14 15:35:09 +0800

    Add choose

--- src/choose.js
+++ src/choose.js
@@ -6,14 +6,21 @@
 
 type Options = {
   test?: string | RegExp,
-  include?: Array<string>,
-  exclude?: Array<string>
+  include?: string | Array<string>,
+  exclude?: string | Array<string>
 }
 
-export default function choose({ test, include, exclude }: Options) {
-  const call = '[object RegExp]' === Object.prototype.toString.call(test)
-        ? (a => test.test(a))
-        : (a => a === String(test))
+export default function choose({ test,
+                                 include = [],
+                                 exclude = [] }: Options = {}) {
+  const call = test
+        ? ('[object RegExp]' === Object.prototype.toString.call(test)
+           ? (a => test.test(a))
+           : (a => a === String(test)))
+        : (() => true)
+
+  const inc = Array.isArray(include) ? include : [include]
+  const exc = Array.isArray(exclude) ? exclude : [exclude]
 
   return function choose1(collects: Array<string>): Array<string> {
     return collects
@@ -30,9 +37,51 @@ export default function choose({ test, include, exclude }: Options) {
 import assert from 'assert'
 
 describe('choose', function() {
+  it('should choose with default options', function() {
+    assert.deepStrictEqual(
+      choose()(['foo', 'bar']),
+      ['foo', 'bar']
+    )
+  })
+
   it('should choose with test string type', function() {
     assert.deepStrictEqual(
       choose({ test: 'foo' })(['foo', 'bar']),
+      ['foo']
+    )
+  })
+
+  it('should choose with test regexp', function() {
+    assert.deepStrictEqual(
+      choose({ test: /^ba/ })(['bar', 'baz']),
+      ['bar', 'baz']
+    )
+  })
+
+  it('should choose with include', function() {
+    assert.deepStrictEqual(
+      choose({ include: ['bar'] })(['foo']),
+      ['foo', 'bar']
+    )
+  })
+
+  it('should choose with include with string type', function() {
+    assert.deepStrictEqual(
+      choose({ include: 'bar' })(['foo']),
+      ['foo', 'bar']
+    )
+  })
+
+  it('should choose with exclude', function() {
+    assert.deepStrictEqual(
+      choose({ exclude: ['bar'] })(['foo', 'bar']),
+      ['foo']
+    )
+  })
+
+  it('should choose with exclude with string type', function() {
+    assert.deepStrictEqual(
+      choose({ exclude: 'bar' })(['foo', 'bar']),
       ['foo']
     )
   })
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,39 @@
+import glob from 'glob'
 import path from 'path'
-import { SourceMapDevToolPlugin } from 'webpack'
 
-export default [{
-  mode: process.env.NODE_ENV,
-  node: false,
-  entry: path.resolve('src/index.js'),
-  output: {
-    path: path.resolve('.'),
-    filename: 'index.js',
-    library: 'Logger',
-    libraryTarget: 'umd',
-    globalObject: 'this'
-  },
-  module: {
-    rules: [{
-      test: /.js$/,
-      use: 'babel-loader'
-    }]
-  },
-  optimization: {
-    minimize: false
-  },
-  plugins: [
+export default glob.sync('./src/*.js').map(file => {
+  const name = path.basename(file, path.extname(file))
+  return {
+    mode: process.env.NODE_ENV,
+    node: false,
+    entry: {
+      [name]: file
+    },
+    output: {
+      path: path.resolve('.'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [{
+        test: /.js$/,
+        use: 'babel-loader'
+      }]
+    },
+    optimization: {
+      minimize: false
+    },
+    plugins: [
 
-  ],
-  externals: [
-    'chalk'
-  ]
-},{
-  mode: process.env.NODE_ENV,
-  entry: path.resolve('src/socket.js'),
-  node: false,
-  output: {
-    path: path.resolve('.'),
-    filename: 'socket.js',
-    library: 'Logger',
-    libraryTarget: 'umd',
-    globalObject: 'this'
-  },
-  module: {
-    rules: [{
-      test: /.js$/,
-      use: 'babel-loader'
-    }]
-  },
-  optimization: {
-    minimize: false
-  },
-  plugins: [
+    ],
+    externals: [
+      (context, request, callback) => {
+        if(Boolean(~request.indexOf(name))) {
+          callback()
+          return
+        }
 
-  ],
-  externals: [
-    'chalk',
-    'isomorphic-ws',
-    './'
-  ]
-},{
-  mode: process.env.NODE_ENV,
-  target: 'node',
-  entry: path.resolve('src/inject-position-plugin.js'),
-  node: false,
-  output: {
-    path: path.resolve('.'),
-    filename: 'inject-position.js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [{
-      test: /.js$/,
-      use: 'babel-loader'
-    }]
-  },
-  optimization: {
-    minimize: false
-  },
-  plugins: [
-
-  ],
-  externals: [
-
-  ]
-}]
+        callback(null, 'commonjs ' + request)
+      }
+    ]
+  }
+})
