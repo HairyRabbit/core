fdaba0c90296f2ee6d86088301e09cd3de2d7898
Rabbit <yfhj1990@hotmail.com>
2018-08-17 15:57:46 +0800

    Fix babel parse error

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * core, utils and kits
+ *
+ * @flow
+ */
+
+export { default as choose } from './src/choose'
+/**
+ * @TODO: babel parse error
+ * see https://github.com/babel/babel/pull/8080
+ */
+// export type { Options as ChooseOptions } from './src/choose'
+
+export { default as ensureArray } from './src/ensureArray'
